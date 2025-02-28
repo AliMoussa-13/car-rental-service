f1670ed6d0644c13e6979ad69d017945c5afada0
Ali Moussa <ali.moussa2004.13>
2025-02-28 14:08:07 -0800

    Adding setter and getter for rental price per day attribute

--- Car_Rental.py
+++ Car_Rental.py
@@ -3,14 +3,18 @@ class Vechile :
         self.brand=brand
         self.model=model
         self.year=year
-        self.rental_price_per_day=rental_price_per_day
+        self.__rental_price_per_day=rental_price_per_day
+    def get_rental_price_per_day(self):
+        return self.__rental_price_per_day
+    def set_rental_price_per_day(self,newPrice):
+        self.__rental_price_per_day=newPrice
     def display_info(self):
        return(
             f"{self.__class__.__name__}:{self.brand} {self.model},Year:{self.year},"
-            f"Renatal Price:${self.rental_price_per_day}/day"
+            f"Renatal Price:${self.get_rental_price_per_day()}/day"
         )
     def calculateRentalCost(self,Days):
-        return Days*int((self.rental_price_per_day))
+        return Days*int((self.get.rental_price_per_day()))
 class Car(Vechile):
     def __init__(self, brand, model, year, rental_price_per_day,seating_capacity):
         self.seating_capacity=seating_capacity
